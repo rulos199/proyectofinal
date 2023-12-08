b24ac1d081acb07f12b8c925cebde9b1b01b75fd
rulos199 <115848015+rulos199@users.noreply.github.com>
2023-12-08 10:07:20 -0500

    finalizado

---
+++ formulario.php/index3.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulario</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="stylephp.css">
+</head>
+<body>
+    <form method="post" autocomplete="off" action="send3.php">
+        <h2>Bienvenido</h2>    
+        <div class="input-group">
+            <div class="input-container">
+                <input type="text" name="name" placeholder="Nombre" required>
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="input-container">
+                <input type="password" name="password" placeholder="Contraseña" required>
+                <i class="fa-solid fa-lock"></i>
+            </div>
+            <div class="input-container">
+                <input type="email" name="email" placeholder="Correo" required>
+                <i class="fa-solid fa-envelope"></i>
+            </div>
+            <a href="#">Terminos y condiciones</a>
+            <input type="submit" name="send" class="btn" value="Enviar">
+        </div>
+    </form>
+    
+    <?php
+        include("send3.php");
+    ?>
+
+</body>
+</html>
---
+++ formulario.php/send3.php
@@ -0,0 +1,48 @@
+<?php
+// Incluir el archivo de conexión
+include("conexion.php");
+
+// Verificar si se ha enviado el formulario
+if(isset($_POST['send'])){
+    // Verificar que los campos no estén vacíos
+    if (
+        !empty($_POST['name']) &&
+        !empty($_POST['password']) &&
+        !empty($_POST['email'])
+    ) {
+        // Obtener y limpiar los valores del formulario
+        $name = trim($_POST['name']);
+        $password = trim($_POST['password']);
+        $email = trim($_POST['email']);
+
+        // Evitar inyecciones SQL utilizando consultas preparadas
+        $consulta = $conex->prepare("INSERT INTO datos (nombre, contraseña, email) VALUES (?, ?, ?)");
+        $consulta->bind_param("sss", $name, $password, $email);
+
+        // Ejecutar la consulta
+        $resultado = $consulta->execute();
+
+        // Verificar el resultado de la consulta
+        if($resultado){
+            // Cerrar la consulta preparada
+            $consulta->close();
+
+            // Redirigir a servicios.html
+            header("Location: /proyectofinal/proyectofinal/bicicleta.html");
+            exit(); // Asegura que el script se detenga después de la redirección
+        } else {
+            ?>
+            <h3 class="error">Ocurrió un error</h3>
+            <?php
+        }
+
+        // Cerrar la consulta preparada
+        $consulta->close();
+
+    } else {
+        ?>
+        <h3 class="error">Llenar todos los campos</h3>
+        <?php
+    }
+}
+?>
